77d02e04b2d8bbde0a32d9c14e5e59acac6d0597
smarr2198 <smarr2198@gmail.com>
2022-04-01 18:14:15 -0500

    took prior project and re structured all models/routes

---
+++ public/js/profile.js
@@ -0,0 +1,72 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#post-name').value.trim();
+  const description = document.querySelector('#post-desc').value.trim();
+
+  if (title && description) {
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({ title, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create post');
+    }
+  }
+};
+
+const newCommentHandler = async (event) => {
+  event.preventDefault();
+
+  const comment = document.querySelector('#comment-desc').value.trim();
+
+  if (comment) {
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({ title }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to create Comment');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+document
+  .querySelector('.new-post-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.post-list')
+  .addEventListener('click', delButtonHandler);
+
+document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', newCommentHandler);
